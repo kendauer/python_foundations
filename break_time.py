ebf77a7f4c22378c2fe29d87215d76c3ed73b65b
Ken Dauer <kendauer@yahoo.com>
2015-02-17 13:10:03 -0500

    Changed variable names and added print statements to tell users when the program started and how many times it ran.

--- break_time.py
+++ break_time.py
@@ -4,8 +4,9 @@ import webbrowser
 total_breaks = 3
 break_count = 0
 
+print ("This program start on "+time.ctime())
 while (break_count < total_breaks):
-    time.sleep(1)
+    time.sleep(10)
     webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    i = i + 1
-print i
+    break_count = break_count + 1
+print "Times ran: " + str(break_count)
